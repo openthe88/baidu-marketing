08a813e33068c219ff6ed6c261a48309f7887340
Jackson <yingjie.du@49you.com>
2023-08-16 17:08:46 +0800

    更新百度搜索流报表接口返回结果

--- model/search/reportv2/getProjectDataResult.go
+++ model/search/reportv2/getProjectDataResult.go
@@ -27,10 +27,10 @@ type ProjectDataInfo struct {
 	Cpm               json.Number `json:"cpm,omitempty" dc:"千次展现消费"`
 	TopPvWinA         json.Number `json:"topPvWinA,omitempty" dc:"上方展现胜出率"`
 	TopPvWinP         json.Number `json:"topPvWinP,omitempty" dc:"上方展现胜出率（精确)"`
-	TopPageViews      int64       `json:"topPageViews,omitempty" dc:"上方位展现"`
-	TopPClicks        int64       `json:"topPClicks,omitempty" dc:"上方位点击"`
-	TopPay            int64       `json:"topPay,omitempty" dc:"上方位消费"`
-	TopFirstPageViews int64       `json:"topFirstPageViews,omitempty" dc:"上方位首位展现"`
+	TopPageViews      json.Number `json:"topPageViews,omitempty" dc:"上方位展现"`
+	TopPClicks        json.Number `json:"topPClicks,omitempty" dc:"上方位点击"`
+	TopPay            json.Number `json:"topPay,omitempty" dc:"上方位消费"`
+	TopFirstPageViews json.Number `json:"topFirstPageViews,omitempty" dc:"上方位首位展现"`
 	TopFirstPvWinA    json.Number `json:"topFirstPvWinA,omitempty" dc:"上方首位胜出率"`
 	TopFirstPvWinP    json.Number `json:"topFirstPvWinP,omitempty" dc:"上方首位胜出率（精确)"`
 }
--- model/search/reportv2/getUserDataResult.go
+++ model/search/reportv2/getUserDataResult.go
@@ -22,12 +22,12 @@ type UserDataInfo struct {
 	Ctr               json.Number `json:"ctr,omitempty" dc:"消费"`
 	Cpc               json.Number `json:"cpc,omitempty" dc:"平均点击价格"`
 	Cpm               json.Number `json:"cpm,omitempty" dc:"千次展现消费"`
-	TopPageViews      int64       `json:"topPageViews,omitempty" dc:"上方位展现"`
-	TopPClicks        int64       `json:"topPClicks,omitempty" dc:"上方位点击"`
-	TopPay            int64       `json:"topPay,omitempty" dc:"上方位消费"`
+	TopPageViews      json.Number `json:"topPageViews,omitempty" dc:"上方位展现"`
+	TopPClicks        json.Number `json:"topPClicks,omitempty" dc:"上方位点击"`
+	TopPay            json.Number `json:"topPay,omitempty" dc:"上方位消费"`
 	TopPvWinA         json.Number `json:"topPvWinA,omitempty" dc:"上方展现胜出率"`
 	TopPvWinP         json.Number `json:"topPvWinP,omitempty" dc:"上方展现胜出率（精确)"`
-	TopFirstPageViews int64       `json:"topFirstPageViews,omitempty" dc:"上方位首位展现"`
+	TopFirstPageViews json.Number `json:"topFirstPageViews,omitempty" dc:"上方位首位展现"`
 	TopFirstPvWinA    json.Number `json:"topFirstPvWinA,omitempty" dc:"上方首位胜出率"`
 	TopFirstPvWinP    json.Number `json:"topFirstPvWinP,omitempty" dc:"上方首位胜出率（精确)"`
 }
